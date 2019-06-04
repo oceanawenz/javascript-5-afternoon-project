9a170b308e0a2742e5d325ba61be4da35439b717
IC Realtime MBP <ICREALTIME@ICR-MacBook-Pro.local>
2019-06-04 16:51:43 -0700

    stuck on the last one in constructor

--- closures.js
+++ closures.js
@@ -21,14 +21,15 @@ function outer() {
   Above you're given a function that returns another function which has a closure over the name variable.
   Invoke outer saving the return value into another variable called 'inner'.
 */
-  
-// Code Here
+
+var inner = outer();
+
 
 
 
 //Once you do that, invoke inner.
 
-//Code Here
+inner();
 
 
 
@@ -51,7 +52,8 @@ function callFriend(name) {
   (HINT: You will need to pass in arguments to both function invocations)
 */
 
-//Code Here
+var callJake = callFriend('Jake');
+// callJake('435-555-9248', 'Jake');
 
 
 
@@ -61,16 +63,20 @@ function callFriend(name) {
   Write a function called makeCounter that makes the following code work properly.
 */
 
-//Code Here
+function makeCounter() {
+  var num = 1;
+  return function() {
+    return num++
+  }
+}
 
 
 
-//Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -86,18 +92,21 @@ function callFriend(name) {
 */
 
 function counterFactory(value) {
-  // Code here.
-
   return {
-
+    inc: function(){
+      return value+= 1
+    },
+    dec: function() {
+      return value-= 1
+    }
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 
@@ -112,13 +121,15 @@ counter = counterFactory(10);
 function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
-  // code message function here.
+  function message() {
+    return welcomeText + " " + firstname + " " + lastname + ".";
+  }
 
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
-var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
+var greeting = motivation('Billy ', 'Bob '); // 'You're doing awesome keep it up Billy Bob.
 
 
 
@@ -143,10 +154,12 @@ var module = (function() {
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
-    // Code here.
+    publicMethod: function() {
+      return privateMethod()
+    }
   };
 })();
-
+module.publicMethod()
 
 
 ////////// PROBLEM 7 //////////
@@ -162,7 +175,12 @@ function secretNumber() {
   var secret = 143;
 
   return {
-    // Code here
+    addToSecret: function(udpatedSecretNumber) {
+      return secret += udpatedSecretNumber
+    },
+    takeAwayFromSecret: function(udpatedSecretNumber) {
+      return secret -= udpatedSecretNumber
+    }
   };
 }
 
@@ -187,10 +205,22 @@ function secretNumber() {
 */
 
 function timeOutCounter() {
-  for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
-      console.log(i);
-    }, i * 1000);
+ for (var i = 0; i <= 5; i++) {
+     function repeat(i) {
+      setTimeout(function() {
+        console.log(i);
+      }, i * 1000);
+     }
+  repeat(i)
   }
 }
 timeOutCounter();
+
+// function timeOutCounter() {
+//   for (var i = 0; i <= 5; i++) {
+//     setTimeout(function() {
+//       console.log(i);
+//     }, i * 1000);
+//   }
+// }
+// timeOutCounter();
